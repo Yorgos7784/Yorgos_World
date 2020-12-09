26e07a3f7ca69c5cdc7f92e4fd05e3afa724fd69
Yorgos7784 <ghkd7784@gmail.com>
2020-12-09 12:29:45 +0900

    20201209 수업자료

--- Java_work/control/src/control/Practice.java
+++ Java_work/control/src/control/Practice.java
@@ -49,17 +49,17 @@ public class Practice {
 		
 		
 		
-		/*System.out.println("--------------------------");
+		System.out.println("--------------------------");
 		System.out.println("     구구단 프로그램 v1.0");
 		System.out.println("--------------------------");
 		
-		for(int i = 2; i <= 9; i++) {
-			for(int j = 1; j <= 9; j++){
+		for(int i = 1; i <= 9; i++) {
+			for(int j = 2; j <= 9; j++){
 				
-				System.out.print(i + " x " + j + " = " + (i*j) + "   ");
+				System.out.print(j + " x " + i + " = " + (i*j) + "	");
 			}
 			System.out.println();
-		}*/
+		}
 		
 		
 		
---
+++ Java_work/if_20201209/src/if_20201209/If1.java
@@ -0,0 +1,15 @@
+package if_20201209;
+
+public class If1 {
+
+	public static void main(String[] args) {
+		
+		int num = 99;
+		if(num < 100) {
+			System.out.println("num가 100보다 작다.");
+		}
+		
+		
+	}
+
+}
